a5669e99a87af6fae6e694e644b79fd323c78c27
msaleemm <asimsaleem@hotmail.com>
2022-11-17 00:15:01 -0500

    Update streamlit_app.py
    added description

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents Happy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boil Free-Range Egg')
